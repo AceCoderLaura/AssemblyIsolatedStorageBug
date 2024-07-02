01e1c08743d1cf03ac34639baf813c79c1ea19b4
Laura Bailey <laurab@adaptsolutions.com.au>
2024-07-02 11:42:51 +1000

    Create repro project

---
+++ ClassLibrary/LibraryClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO.IsolatedStorage;
+
+namespace ClassLibrary
+{
+    public class LibraryClass
+    {
+        public const string Test = "test";
+
+        public static void ReadDir(Func<IsolatedStorageFile> getStore)
+        {
+            var store = getStore();
+            if (store.DirectoryExists(Test))
+            {
+                Console.WriteLine("Store is same, no bug.");
+            }
+            else
+            {
+                Console.WriteLine("STORE IS DIFFERENT THIS IS A BUG!");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,25 @@
-﻿namespace DomainIsolatedStorageBug
+﻿using System.IO.IsolatedStorage;
+using ClassLibrary;
+
+namespace DomainIsolatedStorageBug
 {
-    internal class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
+            CreateDir();
+            LibraryClass.ReadDir(GetStore);
+        }
+
+        public static void CreateDir()
+        {
+            var store = GetStore();
+            store.CreateDirectory(LibraryClass.Test);
+        }
+
+        private static IsolatedStorageFile GetStore()
+        {
+            return IsolatedStorageFile.GetStore(IsolatedStorageScope.User | IsolatedStorageScope.Assembly, null, null);
         }
     }
 }
