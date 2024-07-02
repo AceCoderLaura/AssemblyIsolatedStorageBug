663e8805f6d5d9dd400c3ffad02486024c772638
Laura Bailey <laurab@adaptsolutions.com.au>
2024-07-02 13:45:04 +1000

    Added an option to fix the issue

--- Program.cs
+++ Program.cs
@@ -1,25 +1,37 @@
-﻿using System.IO.IsolatedStorage;
+﻿using System;
+using System.IO.IsolatedStorage;
 using ClassLibrary;
 
 namespace AssemblyIsolatedStorageBug
 {
     public class Program
     {
+        private const bool FixEnabled = false;
+
         public static void Main(string[] args)
         {
             CreateDir();
-            LibraryClass.ReadDir(GetStore);
+            LibraryClass.ReadDir(GetProgramStore);
         }
 
         private static void CreateDir()
         {
-            var store = GetStore();
+            var store = GetProgramStore();
             store.CreateDirectory(LibraryClass.Test);
         }
 
-        private static IsolatedStorageFile GetStore()
+        private static IsolatedStorageFile GetProgramStore()
         {
-            return IsolatedStorageFile.GetStore(IsolatedStorageScope.User | IsolatedStorageScope.Assembly, null, null);
+            var isolatedStorageFile = IsolatedStorageFile.GetStore(IsolatedStorageScope.User | IsolatedStorageScope.Assembly, null, null);
+
+            // Isolated storage appears to use the call stack to determine what assembly is calling GetStore in order
+            // to perform assembly-level isolation for storage. When this line is optimised away, GetProgramStore is
+            // removed from the call stack after GetStore is called, that way the IsolatedStorageFile gets returned
+            // directly to the caller of GetProgramStore, which is obviously faster, but breaks isolated storage's
+            // caller detection.
+            if (FixEnabled) Console.Beep();
+
+            return isolatedStorageFile;
         }
     }
 }
